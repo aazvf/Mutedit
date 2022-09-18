49b79a3daa304c7abfa886b28bc6b7fe1ee24218
Aaron <aaron@machine>
2022-09-18 23:42:42 +0100

    only show comments if there are some

--- components/article/Layout.vue
+++ components/article/Layout.vue
@@ -7,10 +7,13 @@
                 <article-media :article="article" />
                 <div class="px-1 pb-2 leading-7">
                     <article-comments-fetch-buttons :article="article" />
-
                     <article-meta-badges :article="article" />
                 </div>
-                <article-comments :article="article" class="mb-1" />
+                <article-comments
+                    :article="article"
+                    class="mb-1"
+                    v-if="article.comments.length > 0"
+                />
             </template>
             <template #message>
                 <span :class="$theme.text3">[{{ article.data.subreddit.toLowerCase() }}]</span>
